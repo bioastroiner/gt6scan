05d4821e0d5522934a93018733b14d2819d2cef6
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2023-08-14 17:28:02 +0330

    Electric Tools use Duribility Fix

---
+++ src/main/java/bioast/mods/gt6m/mixin/MultiItemToolMixin.java
@@ -0,0 +1,37 @@
+package bioast.mods.gt6m.mixin;
+
+import bioast.mods.gt6m.GT6M_Mod;
+import gregapi.data.TD;
+import gregapi.item.IItemEnergy;
+import gregapi.item.multiitem.MultiItemTool;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import static gregapi.data.CS.T;
+
+@Mixin(value = MultiItemTool.class, remap = false)
+public class MultiItemToolMixin {
+    /*
+    If a tool uses Energy do not use Duribility anymore!
+    */
+    @Inject(method = "doDamage(Lnet/minecraft/item/ItemStack;JLnet/minecraft/entity/EntityLivingBase;Z)Z"
+            , at = @At(value = "INVOKE", target = "Lgregapi/item/multiitem/MultiItemTool;getEnergyStats(Lnet/minecraft/item/ItemStack;)Lgregapi/item/IItemEnergy;", shift = At.Shift.AFTER),
+            cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    public void doDamage(ItemStack aStack,
+                         long aAmount,
+                         EntityLivingBase aPlayer,
+                         boolean aAllowBreaking,
+                         CallbackInfoReturnable<Boolean> cir,
+                         IItemEnergy tElectric) {
+        if(tElectric != null /*&& Config*/) {
+            GT6M_Mod.LOG.debug("Electric tool was detected, Injecting Damage Override");
+            cir.setReturnValue(((MultiItemTool) (Object) this).useEnergy(TD.Energy.EU, aStack, aAmount, aPlayer, null, null, 0, 0, 0, T));
+        }
+        GT6M_Mod.LOG.debug("Electric tool was NOT detected, Injected Nothing");
+    }
+}
