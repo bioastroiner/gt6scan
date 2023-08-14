78018a02be75fc5bf00583dd4c068ff913e8dafc
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2023-08-14 17:27:45 +0330

    Scrap and Penalty

---
+++ src/main/java/bioast/mods/gt6m/mixin/GT_ToolStats_Mixin.java
@@ -0,0 +1,40 @@
+package bioast.mods.gt6m.mixin;
+
+import bioast.mods.gt6m.Config;
+import bioast.mods.gt6m.GT6M_Mod;
+import bioast.mods.gt6m.helper.ToolHelper;
+import gregapi.item.IItemEnergy;
+import gregapi.item.multiitem.MultiItemTool;
+import gregapi.item.multiitem.MultiItemToolWithCompat;
+import gregapi.item.multiitem.tools.ToolStats;
+import gregtech.items.MultiItemRandomTools;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = ToolStats.class,remap = false)
+public class GT_ToolStats_Mixin {
+
+    //    @Shadow()
+//    private long mMaterialAmount;
+    @Inject(method = "getBrokenItem(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;", at = @At("RETURN"), cancellable = true)
+    public void getBrokenItem(ItemStack aStack, CallbackInfoReturnable<ItemStack> cir) {
+        //ItemStack stack = mMaterialAmount < U4 ? null : OP.scrapGt.mat(MultiItemTool.getPrimaryMaterial(aStack), 1 + RNGSUS.nextInt(1 + (int) (4 * mMaterialAmount / U)));
+        GT6M_Mod.LOG.debug("No Scraps Added!");
+        if(Config.PENALTY_NO.getBool())  cir.setReturnValue(null);
+    }
+
+    @Inject(method = "afterBreaking(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/EntityPlayer;)V", at = @At("HEAD"), cancellable = true)
+    public void afterBreaking(ItemStack aStack, EntityPlayer aPlayer, CallbackInfo ci) {
+        // Greg: "If you work so hard that your Tool breaks, you should probably take a break yourself. :P"
+        // Bio: No :3
+        GT6M_Mod.LOG.debug("No More Nasty Potion Effects");
+        if(Config.SCRAP_NO.getBool())
+            ci.cancel();
+
+    }
+}
---
+++ src/main/java/bioast/mods/gt6m/mixin/MixinPlugin.java
@@ -0,0 +1,45 @@
+package bioast.mods.gt6m.mixin;
+
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import org.spongepowered.libraries.org.objectweb.asm.tree.ClassNode;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return false;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+}
