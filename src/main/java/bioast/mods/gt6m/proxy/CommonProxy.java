2a8068503b8ecfe90099eae04bc4dff4a3bf3a1f
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2023-08-22 22:25:46 +0330

    Remove Old Bullshit code

--- src/main/java/bioast/mods/gt6m/proxy/CommonProxy.java
+++ src/main/java/bioast/mods/gt6m/proxy/CommonProxy.java
@@ -1,22 +1,16 @@
 package bioast.mods.gt6m.proxy;
 
 import bioast.mods.gt6m.Config;
-import bioast.mods.gt6m.scanner.NetwrokHandler;
-import bioast.mods.gt6m.scanner.gui.ScannerGUI;
-import bioast.mods.gt6m.scanner.utils.VALs;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.network.IGuiHandler;
 import gregapi.api.Abstract_Proxy;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
-public class CommonProxy extends Abstract_Proxy implements IGuiHandler {
+public class CommonProxy extends Abstract_Proxy {
 
     CommonProxy() {
         MinecraftForge.EVENT_BUS.register(this);
@@ -31,8 +25,6 @@ public class CommonProxy extends Abstract_Proxy implements IGuiHandler {
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
-//        FMLCommonHandler.instance().bus().register(this);
-        new NetwrokHandler();
     }
 
     // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
@@ -46,28 +38,6 @@ public class CommonProxy extends Abstract_Proxy implements IGuiHandler {
     @SubscribeEvent
     public void onPlayerUseEvent(PlayerInteractEvent aEvent) {
         if (aEvent.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK) {
-//            World aWorld = aEvent.world;
-//            Block tBlock = aEvent.world.getBlock(aEvent.x, aEvent.y, aEvent.z);
-//            short tMeta = (short) aWorld.getBlockMetadata(aEvent.x, aEvent.y, aEvent.z);
-//            UT.Entities.sendchat(aEvent.entityPlayer,"ORE: "+HLPs.ore(tBlock,tMeta));
-//            UT.Entities.sendchat(aEvent.entityPlayer,"MAT: "+HLPs.oreId(tBlock,aWorld.getTileEntity(aEvent.x, aEvent.y, aEvent.z)));
-//
         }
-    }
-
-    @Override
-    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-        if (ID == VALs.GUI_ID) return null;
-        return null;
-    }
-
-    @Override
-    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-        if (ID == VALs.GUI_ID) return new ScannerGUI();
-        return null;
-    }
-
-    public void openProspectorGUI() {
-
     }
 }
--- src/main/java/bioast/mods/gt6m/scanner/item/ScannerToolStats.java
+++ src/main/java/bioast/mods/gt6m/scanner/item/ScannerToolStats.java
@@ -1,13 +1,11 @@
 package bioast.mods.gt6m.scanner.item;
 
-import bioast.mods.gt6m.scanner.item.behaviour.ScannerBehavior;
 import gregapi.item.multiitem.MultiItemTool;
 import gregapi.item.multiitem.tools.ToolStats;
 import gregapi.old.Textures;
 import gregapi.render.IIconContainer;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
 
 public class ScannerToolStats extends ToolStats {
     public final int tier;
@@ -17,8 +15,8 @@ public class ScannerToolStats extends ToolStats {
     }
 
     public float getMaxDurabilityMultiplier() {
-        if (tier - 6 == 0) return (float) Math.pow(((float) ((tier - 6F) * 2F)), 0.0D);
-        else return (float) ((tier - 6F) * 2F);
+        if (tier - 6 == 0) return (float) Math.pow((tier - 6F) * 2F, 0.0D);
+        else return (tier - 6F) * 2F;
     }
 
     @Override
@@ -29,12 +27,7 @@ public class ScannerToolStats extends ToolStats {
     @Override
     public short[] getRGBa(boolean aIsToolHead, ItemStack aStack) {
         return aIsToolHead ? MultiItemTool.getPrimaryMaterial(aStack).mRGBaSolid
-                : MultiItemTool.getSecondaryMaterial(aStack).mRGBaSolid;
-    }
-
-    @Override
-    public void onStatsAddedToTool(MultiItemTool aItem, int aID) {
-        aItem.addItemBehavior(aID, new ScannerBehavior());
+            : MultiItemTool.getSecondaryMaterial(aStack).mRGBaSolid;
     }
 
     @Override
