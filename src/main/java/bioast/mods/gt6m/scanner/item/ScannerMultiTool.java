7bd0e70d96ddb8a18dc99b62030d8b8d6353a32e
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2023-08-28 22:05:56 +0330

    working menu

--- src/main/java/bioast/mods/gt6m/scanner/item/ScannerMultiTool.java
+++ src/main/java/bioast/mods/gt6m/scanner/item/ScannerMultiTool.java
@@ -6,13 +6,18 @@ import bioast.mods.gt6m.scanner.network.OreDataSyncHandler;
 import bioast.mods.gt6m.scanner.utils.VALs;
 import com.cleanroommc.modularui.api.IItemGuiHolder;
 import com.cleanroommc.modularui.api.drawable.IDrawable;
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.api.widget.IWidget;
 import com.cleanroommc.modularui.drawable.GuiDraw;
+import com.cleanroommc.modularui.drawable.ItemDrawable;
 import com.cleanroommc.modularui.manager.GuiInfos;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.ModularScreen;
 import com.cleanroommc.modularui.sync.GuiSyncHandler;
 import com.cleanroommc.modularui.sync.SyncHandlers;
-import com.cleanroommc.modularui.widget.Widget;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.widgets.TextWidget;
+import com.cleanroommc.modularui.widgets.layout.Grid;
 import gregapi.block.prefixblock.PrefixBlock;
 import gregapi.block.prefixblock.PrefixBlockTileEntity;
 import gregapi.data.MT;
@@ -30,6 +35,7 @@ import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.Chunk;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -38,6 +44,7 @@ import static bioast.mods.gt6m.scanner.utils.HLPs.prefixBlock;
 
 public class ScannerMultiTool extends MultiItemTool implements IItemGuiHolder {
     List<OreData> scannedOres = new ArrayList<>();
+    Map<Short, Integer> sortedOres = new HashMap<>();
     int chunkSize = 9;
     int oresFound = 0;
     int x_origin;
@@ -71,50 +78,8 @@ public class ScannerMultiTool extends MultiItemTool implements IItemGuiHolder {
         return super.onItemRightClick(aStack, aWorld, aPlayer);
     }
 
-    private void clientLogic(ItemStack aStack, WorldClient aWorld, EntityPlayer aPlayer) {
-        /* CLIENT CODE */
-        int borderColor = col(MT.Gray);
-        int backgroundColor = col(MT.White);
-        int oreColor = 0;
-        for (int chunkGridX = 0; chunkGridX < chunkSize; chunkGridX++) {
-            for (int chunkGridZ = 0; chunkGridZ < chunkSize; chunkGridZ++) {
-                for (int z = 0; z < 16; z++) {
-                    for (int x = 0; x < 16; x++) {
-                        int blockGridX, blockGridZ;
-                        boolean isOre = false;
-                        blockGridX = x + chunkGridX * 16;
-                        blockGridZ = z + chunkGridZ * 16;
-                        int blockWorldX = x_origin + blockGridX;
-                        int blockWorldZ = z_origin + blockGridZ;
-                        int lastY = 0;
-                        try {
-                            if (scannedOres != null && !scannedOres.isEmpty())
-                                for (OreData data : scannedOres) {
-                                    if (data.x == blockWorldX && data.z == blockWorldZ) {
-                                        if (lastY < data.y) {
-                                            oreColor = col(OreDictMaterial.MATERIAL_ARRAY[data.matID]);
-                                            lastY = data.y;
-                                        }
-                                        isOre = true;
-                                    }
-                                }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-
-                        block[blockGridX][blockGridZ] = backgroundColor;
-                        if (isOre) {
-                            block[blockGridX][blockGridZ] = oreColor;
-                        }
-                        if (x == 15 || z == 15 || x == 0 || z == 0) // We Skip 16th block to draw Borders
-                            block[blockGridX][blockGridZ] = borderColor;
-                        if (chunkGridZ == (chunkSize - 1) / 2 && chunkGridX == (chunkSize - 1) / 2 && x == 7 && z == 7)
-                            block[blockGridX][blockGridZ] = col(MT.Red);
-                    }
-                }
-            }
-        }
-        GuiInfos.PLAYER_ITEM_MAIN_HAND.open(aPlayer);
+    private static IWidget wItem(OreDictMaterial mat) {
+        return new ItemDrawable().setItem(OP.oreRaw.dat(mat).getStack(1)).asWidget();
     }
 
     private void serverLogic(ItemStack aStack, WorldServer aWorld, EntityPlayer aPlayer, List<String> chat) {
@@ -166,25 +131,89 @@ public class ScannerMultiTool extends MultiItemTool implements IItemGuiHolder {
         }
     }
 
+    private void clientLogic(ItemStack aStack, WorldClient aWorld, EntityPlayer aPlayer) {
+        /* CLIENT CODE */
+        int borderColor = col(MT.Gray);
+        int backgroundColor = col(MT.White);
+        int oreColor = 0;
+        sortedOres.clear();
+        for (int chunkGridX = 0; chunkGridX < chunkSize; chunkGridX++) {
+            for (int chunkGridZ = 0; chunkGridZ < chunkSize; chunkGridZ++) {
+                for (int z = 0; z < 16; z++) {
+                    for (int x = 0; x < 16; x++) {
+                        int blockGridX, blockGridZ;
+                        boolean isOre = false;
+                        blockGridX = x + chunkGridX * 16;
+                        blockGridZ = z + chunkGridZ * 16;
+                        int blockWorldX = x_origin + blockGridX;
+                        int blockWorldZ = z_origin + blockGridZ;
+                        int lastY = 0;
+                        try {
+                            if (scannedOres != null && !scannedOres.isEmpty())
+                                for (OreData data : scannedOres) {
+                                    if (data.x == blockWorldX && data.z == blockWorldZ) {
+                                        if (lastY < data.y) {
+                                            oreColor = col(OreDictMaterial.MATERIAL_ARRAY[data.matID]);
+                                            lastY = data.y;
+                                        }
+                                        if (sortedOres.containsKey(data.matID)) {
+                                            int lastCount = sortedOres.get(data.matID);
+                                            sortedOres.put(data.matID, lastCount + 1);
+                                        } else sortedOres.put(data.matID, 1);
+                                        isOre = true;
+                                    }
+                                }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        block[blockGridX][blockGridZ] = backgroundColor;
+                        if (isOre) {
+                            block[blockGridX][blockGridZ] = oreColor;
+                        }
+                        if (x == 15 || z == 15 || x == 0 || z == 0) // We Skip 16th block to draw Borders
+                            block[blockGridX][blockGridZ] = borderColor;
+                        if (chunkGridZ == (chunkSize - 1) / 2 && chunkGridX == (chunkSize - 1) / 2 && x == 7 && z == 7)
+                            block[blockGridX][blockGridZ] = col(MT.Red);
+                    }
+                }
+            }
+        }
+        GuiInfos.PLAYER_ITEM_MAIN_HAND.open(aPlayer);
+    }
 
     @Override
     public ModularScreen createGuiScreen(EntityPlayer player, ItemStack itemStack) {
-        return ModularScreen.simple("ores_screen", guiContext -> {
+        return ModularScreen.simple("map", guiContext -> {
             ModularPanel panel = ModularPanel.defaultPanel(guiContext);
-            Widget mapWidget = ((IDrawable) (context, x, y, width, height) -> {
-                //if(hasDrawn) return;
+            panel.flex().size(300, 166).align(Alignment.Center);
+            IWidget mapWidget = ((IDrawable) (context, x, y, width, height) -> {
+                // TODO get rid of this for loop it would be a HUGE deal to optimization
                 for (int i = 0; i < 16 * chunkSize; i++) {
                     for (int j = 16 * chunkSize - 1; j > 0; j--) {
                         if (block[i][j] == col(MT.White)) continue;
                         GuiDraw.drawRect(i, j, 1, 1, block[i][j]);
                     }
-                    //                        for (int j = 0; j < 16*chunkSize; j++) {
-                    //                            //if (block[i][j] == col(MT.White)) continue;
-                    //                            GuiDraw.drawRect(i, j, 1, 1, block[i][j]);
-                    //                        }
                 }
             }).asWidget().pos(10, 10).right(10).bottom(10);
+//            IWidget itemWidget = new ItemDrawable().setItem(OP.ore.mat(MT.Pt,1)).asWidget();
+//            IWidget textWidget = new TextWidget(IKey.str(MT.Pt.mNameLocal));
+//            List<IWidget> widgetRow = new ArrayList<>();
+//            widgetRow.add(itemWidget);
+//            widgetRow.add(textWidget);
+//            List<List<IWidget>> widgetMatrix = new ArrayList<>();
+//            widgetMatrix.add(widgetRow);
+            Grid listWidget = new Grid().scrollable().size(150, 150).pos(280 - 125, 0);
+//                .row(wItem(MT.Pt),new TextWidget(IKey.str(MT.Pt.mNameLocal + ": " + String.valueOf(100000))).color(UT.Code.getRGBaInt((MT.Pt.fRGBaSolid))))
+//                .row(wItem(MT.Azurite),new TextWidget(IKey.str(MT.Azurite + ": " + String.valueOf(256))));
+            sortedOres.forEach((matID, amount) -> {
+                OreDictMaterial mat = OreDictMaterial.MATERIAL_ARRAY[matID];
+                IWidget itemWidget = wItem(mat);
+                IWidget nameWidget = new TextWidget(IKey.str(mat.mNameLocal + ": " + amount)).color(UT.Code.getRGBaInt((mat.fRGBaSolid)));
+                listWidget.row(itemWidget, nameWidget).nextRow();
+            });
             panel.child(mapWidget);
+            panel.child(listWidget);
             return panel;
         });
     }
