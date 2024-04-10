85543c6777118b722d23d893192ed939f8c45845
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2024-04-10 23:45:12 +0330

    + JEI support

--- src/main/java/bioast/mods/gt6scan/network/scanmessage/HandlerClient.java
+++ src/main/java/bioast/mods/gt6scan/network/scanmessage/HandlerClient.java
@@ -16,6 +16,7 @@ import com.cleanroommc.modularui.utils.Color;
 import com.cleanroommc.modularui.value.sync.GuiSyncManager;
 import com.cleanroommc.modularui.widgets.TextWidget;
 import com.cleanroommc.modularui.widgets.layout.Grid;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
@@ -24,11 +25,13 @@ import cpw.mods.fml.relauncher.SideOnly;
 import gregapi.data.MT;
 import gregapi.oredict.OreDictMaterial;
 import gregapi.util.UT;
+import journeymap.client.model.Waypoint;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
 import java.util.ArrayList;
@@ -124,84 +127,128 @@ public class HandlerClient implements IMessageHandler<ScanResponse, IMessage>, I
 		}
 	}
 
-    @Override
-    public ModularScreen createScreen(GuiCreationContext guiContext, ModularPanel mainPanel) {
-        ItemStack itemStack = guiContext.getMainHandItem();
-        EntityPlayer player = guiContext.getPlayer();
-        mainPanel.flex().align(Alignment.Center).size(300, 166);
-        IWidget mapWidget = ((IDrawable) (context, x, y, width, height) -> {
-            GL11.glDisable(GL11.GL_TEXTURE_2D);
-            GL11.glEnable(GL11.GL_BLEND);
-            GL11.glDisable(GL11.GL_ALPHA_TEST);
-            OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
-            for (int i = 0; i < 16 * chunkSize; i++) {
-                for (int j = 16 * chunkSize - 1; j >= 0; j--) {
-                    if (blockColorStorage[i][j] == col(MT.White)) continue;
-                    GL11.glShadeModel(GL11.GL_SMOOTH);
-                    int color = blockColorStorage[i][j];
-                    Tessellator.instance.startDrawingQuads();
-                    bufferbuilder.pos(i, j, 0.0f)
-                        .color(Color.getRed(color), Color.getGreen(color), Color.getBlue(color), Color.getAlpha(color))
-                        .endVertex();
-                    bufferbuilder.pos(i, j + 1, 0.0f)
-                        .color(Color.getRed(color), Color.getGreen(color), Color.getBlue(color), Color.getAlpha(color))
-                        .endVertex();
-                    bufferbuilder.pos(i + 1, j + 1, 0.0f)
-                        .color(Color.getRed(color), Color.getGreen(color), Color.getBlue(color), Color.getAlpha(color))
-                        .endVertex();
-                    bufferbuilder.pos(i + 1, j, 0.0f)
-                        .color(Color.getRed(color), Color.getGreen(color), Color.getBlue(color), Color.getAlpha(color))
-                        .endVertex();
-                    Tessellator.instance.draw();
-                }
-            }
-            int mouse_x = mainPanel.getScreen().context.getAbsMouseX();
-            int mouse_y = mainPanel.getScreen().context.getAbsMouseY();
-            GL11.glShadeModel(GL11.GL_FLAT);
-            GL11.glDisable(GL11.GL_BLEND);
-            GL11.glEnable(GL11.GL_ALPHA_TEST);
-            GL11.glEnable(GL11.GL_TEXTURE_2D);
+	@Override
+	public ModularScreen createScreen(GuiCreationContext guiContext, ModularPanel mainPanel) {
+		ItemStack itemStack = guiContext.getMainHandItem();
+		EntityPlayer player = guiContext.getPlayer();
+		mainPanel.flex().align(Alignment.Center).size(300, 166);
+		IWidget mapWidget = ((IDrawable) (context, x, y, width, height) -> {
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			GL11.glEnable(GL11.GL_BLEND);
+			GL11.glDisable(GL11.GL_ALPHA_TEST);
+			OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+			for (int i = 0; i < 16 * chunkSize; i++) {
+				for (int j = 16 * chunkSize - 1; j >= 0; j--) {
+					if (blockColorStorage[i][j] == col(MT.White)) continue;
+					GL11.glShadeModel(GL11.GL_SMOOTH);
+					int color = blockColorStorage[i][j];
+					Tessellator.instance.startDrawingQuads();
+					bufferbuilder.pos(i, j, 0.0f)
+							.color(Color.getRed(color),
+									Color.getGreen(color),
+									Color.getBlue(color),
+									Color.getAlpha(color))
+							.endVertex();
+					bufferbuilder.pos(i, j + 1, 0.0f)
+							.color(Color.getRed(color),
+									Color.getGreen(color),
+									Color.getBlue(color),
+									Color.getAlpha(color))
+							.endVertex();
+					bufferbuilder.pos(i + 1, j + 1, 0.0f)
+							.color(Color.getRed(color),
+									Color.getGreen(color),
+									Color.getBlue(color),
+									Color.getAlpha(color))
+							.endVertex();
+					bufferbuilder.pos(i + 1, j, 0.0f)
+							.color(Color.getRed(color),
+									Color.getGreen(color),
+									Color.getBlue(color),
+									Color.getAlpha(color))
+							.endVertex();
+					Tessellator.instance.draw();
+				}
+			}
+			GL11.glShadeModel(GL11.GL_FLAT);
+			GL11.glDisable(GL11.GL_BLEND);
+			GL11.glEnable(GL11.GL_ALPHA_TEST);
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
+			// convert gui coordinates to world coordinates
+			int mouse_x = mainPanel.getScreen().context.getAbsMouseX();
+			int mouse_y = mainPanel.getScreen().context.getAbsMouseY();
+			int world_x = (mouse_x - mainPanel.getArea().x - x + x_origin - 9);
+			int world_z = (mouse_y - mainPanel.getArea().y - y + z_origin - 9);
+			String corX = "X -> " + world_x;
+			String corZ = "Z -> " + world_z;
+			String corN = " ";
+			int text_color = col(MT.White);
+			try {
+				short mat = blockMatStorage[(mouse_x - mainPanel.getArea().x - x - 10)][(mouse_y - mainPanel.getArea().y - y - 10)];
+				if (mat != 0) {
+					corN = OreDictMaterial.MATERIAL_ARRAY[mat].mNameLocal;
+					text_color = col(OreDictMaterial.MATERIAL_ARRAY[mat]);
+				} else corN = "NaN";
+			} catch (Exception e) {
+				corN = "NaN";
+			}
+			if (Mouse.isButtonDown(0)) {
+				bookmark(world_x, world_z, text_color);
+			}
+			if (Mouse.isButtonDown(1)) {
+				if (!corN.equalsIgnoreCase("nan")) {
+					UT.Entities.sendchat(player,
+							String.format("x: %d, z: %d, has material %s", world_x, world_z, corN));
+				}
+			}
+			String cor = corX + " , " + corZ + " , " + corN + ". Dim: " + player.worldObj.provider.getDimensionName() + ". Mode:" + mode.name()
+					.toLowerCase();
+			GuiDraw.drawText(cor, 0, -15, 1f, text_color, true);
+		}).asWidget().pos(10, 10).right(10).bottom(10);
+		Grid listWidget = new Grid().scrollable().size(150, 150).pos(280 - 125, 0);
+		sortedOres.forEach((matID, amount) -> {
+			OreDictMaterial mat = OreDictMaterial.MATERIAL_ARRAY[matID];
+			IWidget itemWidget = wItem(mat, mode);
+			String name = mat.mNameLocal;
+			int color = UT.Code.getRGBaInt((mat.fRGBaSolid));
+			if (mat == MT.MethaneIce) {
+				color = UT.Code.getRGBaInt(MT.Milk.fRGBaSolid);
+				name = "Natural Gas";
+			}
+			IWidget nameWidget = new TextWidget(IKey.str(name + ": " + amount)).color(color).shadow(true);
+			listWidget.row(itemWidget, nameWidget).nextRow();
+		});
+		mainPanel.child(mapWidget);
+		mainPanel.child(listWidget);
+		return IGuiHolder.super.createScreen(guiContext, mainPanel);
+	}
 
-            String corX = "X -> " + (mouse_x - mainPanel.getArea().x - x + x_origin - 9);
-            String corZ = "Z -> " + (mouse_y - mainPanel.getArea().y - y + z_origin - 9);
-            String corN = " ";
-            int text_color = col(MT.White);
-            try {
-                short mat = blockMatStorage[(mouse_x - mainPanel.getArea().x - x - 10)][(mouse_y - mainPanel.getArea().y - y - 10)];
-                if (mat != 0) {
-                    corN = OreDictMaterial.MATERIAL_ARRAY[mat].mNameLocal;
-                    text_color = col(OreDictMaterial.MATERIAL_ARRAY[mat]);
-                } else corN = "NaN";
-            } catch (Exception e) {
-                corN = "NaN";
-            }
-            String cor = corX + " , " + corZ + " , " + corN + ". Dim: " + player.worldObj.provider.getDimensionName() + ". Mode:" + mode.name()
-                .toLowerCase();
-            GuiDraw.drawText(cor, 0, -15, 1f, text_color, true);
-        }).asWidget().pos(10, 10).right(10).bottom(10);
-        Grid listWidget = new Grid().scrollable().size(150, 150).pos(280 - 125, 0);
-        sortedOres.forEach((matID, amount) -> {
-            OreDictMaterial mat = OreDictMaterial.MATERIAL_ARRAY[matID];
-            IWidget itemWidget = wItem(mat, mode);
-            String name = mat.mNameLocal;
-            int color = UT.Code.getRGBaInt((mat.fRGBaSolid));
-            if (mat == MT.MethaneIce) {
-                color = UT.Code.getRGBaInt(MT.Milk.fRGBaSolid);
-                name = "Natural Gas";
-            }
-            IWidget nameWidget = new TextWidget(IKey.str(name + ": " + amount)).color(color).shadow(true);
-            listWidget.row(itemWidget, nameWidget).nextRow();
-        });
-        mainPanel.child(mapWidget);
-        mainPanel.child(listWidget);
-        //mainPanel.child(Wi)
-//        mainPanel.child(new ButtonWidget<>().align(Alignment.TopLeft).onMousePressed(mouseButton -> {
-//            refresh();return true;}));
-        return IGuiHolder.super.createScreen(guiContext, mainPanel);
-    }
+	private boolean bookmark(int world_x, int world_z, int color) {
+		// bookmark on a WayPoint manager
+		//todo antique atlas
+		if (Loader.isModLoaded("journeymap")) {
+			Waypoint waypoint = Waypoint.at(
+					world_x,
+					Minecraft.getMinecraft().thePlayer.serverPosY,
+					world_z,
+					Waypoint.Type.Normal,
+					Minecraft.getMinecraft().thePlayer.dimension);
+			if (color < 0) {
+				waypoint.setRandomColor();
+			} else {
+				waypoint.setColor(color);
+			}
+			return true;
+		} else if (Loader.isModLoaded("xaero")) {
+			//todo Xaer
+		}
+		return false;
+	}
 
-    @Override
-    public ModularPanel buildUI(GuiCreationContext creationContext, GuiSyncManager syncManager, boolean isClient) {
-        return ModularPanel.defaultPanel("Scanner");
-    }
+	@Override
+	public ModularPanel buildUI(GuiCreationContext creationContext, GuiSyncManager syncManager, boolean isClient) {
+		return ModularPanel.defaultPanel("Scanner");
+	}
+
+	private boolean bookmark(int world_x, int world_z) {return bookmark(world_x, world_z, -1);}
 }
