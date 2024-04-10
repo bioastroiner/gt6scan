50cd9197d546f0b5179847b78f9b612d6a90fb1c
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2024-04-10 22:26:08 +0330

    + Help Command

--- src/main/java/bioast/mods/gt6scan/network/ScanCommand.java
+++ src/main/java/bioast/mods/gt6scan/network/ScanCommand.java
@@ -2,6 +2,7 @@ package bioast.mods.gt6scan.network;
 
 import bioast.mods.gt6scan.network.scanmessage.ScanRequest;
 import bioast.mods.gt6scan.proxy.CommonProxy;
+import gregapi.data.LH;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
@@ -37,6 +38,9 @@ public class ScanCommand extends CommandBase {
             } else if (args.length > 1) {
                 if (NumberUtils.isNumber(args[0])) {
                     size = Integer.parseInt(args[1]);
+                    scan(sender, size, mode, x, z);
+                } else if (args[0].equals("help")) {
+                    sender.addChatMessage(new ChatComponentText(help()));
                 } else {
                     var res = Arrays.stream(ScanMode.values())
                         .map(ScanMode::toString)
@@ -51,24 +55,40 @@ public class ScanCommand extends CommandBase {
                             size = Integer.parseInt(args[1]);
                         }
                     }
+                    scan(sender, size, mode, x, z);
                 }
             }
-            if (size > 9) {
-                sender.addChatMessage(new ChatComponentText(String.format("Size Capped to 9, from %d", size)));
-                size = 9;
-            }
-            sender.addChatMessage(new ChatComponentText(String.format(
-                "Beginning Scanning Mode:%s, at X:%d Z:%d with size:{%d}",
-                mode,
-                x,
-                z,
-                size)));
-            CommonProxy.simpleNetworkWrapper.sendToServer(new ScanRequest(mode, x, z, size));
         }
+    }
+
+    private static void scan(ICommandSender sender, int size, ScanMode mode, int x, int z) {
+        if (size > 9) {
+            sender.addChatMessage(new ChatComponentText(String.format("Size Capped to 9, from %d", size)));
+            size = 9;
+        }
+        sender.addChatMessage(new ChatComponentText(String.format(
+            "Beginning Scanning Mode:%s, at X:%d Z:%d with size:{%d}",
+            mode,
+            x,
+            z,
+            size)));
+        CommonProxy.simpleNetworkWrapper.sendToServer(new ScanRequest(mode, x, z, size));
+    }
+
+    private String help() {
+        return LH.Chat.GOLD + "======================" +
+            LH.Chat.PURPLE + "How to Use /Scan Command for gt6 scanner mod" +
+            LH.Chat.DGRAY + "Feel Free to hop on https://github.com/bioastroiner/gt6scan/issues for bugs" +
+            LH.Chat.GOLD + "/Scan [Number]" +
+            LH.Chat.GRAY + "Scans area around you in the given radius, it is capped at 9 and it defaults mode to: " + LH.Chat.PURPLE + "Large" +
+            LH.Chat.GOLD + "======================";
     }
 
     @Override
     public List addTabCompletionOptions(ICommandSender sender, String[] args) {
+        if (args.length == 1) {
+            return getListOfStringsMatchingLastWord(args, "help", "1", "3", "5", "7", "9");
+        }
         if (args.length > 1) {
             return getListOfStringsMatchingLastWord(args, Arrays.toString(ScanMode.values()));
         }
